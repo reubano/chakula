9b5a1fe7aacb1c7f222ea0c172b9d3c2d4fbac32
Reuben Cummings <reubano@gmail.com>
2017-03-02 11:22:39 +0300

    Bump to version 0.8.0

--- chakula/__init__.py
+++ chakula/__init__.py
@@ -12,7 +12,7 @@ from bisect import bisect
 import feedparser
 import pygogo as gogo
 
-__version__ = '0.7.4'
+__version__ = '0.8.0'
 __title__ = 'chakula'
 __package_name__ = 'chakula'
 __author__ = 'Reuben Cummings'
